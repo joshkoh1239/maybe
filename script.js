2defe967bf330fc2ab8bda4e724b0b1127e76020
joshkoh1239 <152137204+joshkoh1239@users.noreply.github.com>
2024-01-13 10:23:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 15th!";
+  question.innerHTML = "Yay! my number is 571-294-2199 tell me when ur free :D";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
